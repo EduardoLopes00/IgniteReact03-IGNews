6140256ce8c31d1e30bb658cd4cd617a92289a5a
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2021-12-27 19:01:30 -0300

    Configuring access to FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import {query as q} from 'faunadb'
+
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
     providers: [
@@ -12,5 +16,42 @@ export default NextAuth({
                 }
             }
         })
-    ]
+    ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user;
+            
+            try {
+                //Query that verify wheter the user exists and in case of negative, insert the new user. Otherwise, get the information about it.
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+            } catch(err) {
+                console.log(err)
+                return false;
+            }
+            
+            
+            return true;
+        },
+    }
 })
