fd72d92b41376b7fa5b4e65cfde15d542c75211e
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2021-12-27 21:47:30 -0300

    Treating the customer duplication during the checkout request

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+import { fauna } from '../../services/fauna';
+import { query as q } from 'faunadb'
 import { stripe } from '../../services/stripe'
+
+type User = {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_customer_id: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
@@ -8,15 +19,41 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         //Get the informations of the session through the token stored on the cookie. By passing only the 'req' as param, the method can find the token in its header.
         const session = await getSession({req})
 
-        //Make the request to stripe to create a new customer
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            // metadata: 
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
 
+        let customerId = user.data.stripe_customer_id
+
+        if (!customerId) {
+
+            //Make the request to stripe to create a new customer
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                // metadata: 
+            })
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id
+        }
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id, //The ID will be generated through the create method on Stripe
+            customer: customerId, //The ID will be generated through the create method on Stripe
             payment_method_types: ['card'],
             billing_address_collection: 'required', //Define wheter the address will be necessary
             line_items: [
