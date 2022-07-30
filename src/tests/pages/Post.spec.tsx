6082845bad36621313619fa7f85fb5c97815630e
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-07-30 01:48:21 -0300

    Create the initial post test

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -13,6 +13,11 @@ const post = {
   updatedAt: 'july, 30',
 }
 
+const req = null
+const params = {
+  slug: 'my-slug-mocked',
+}
+
 jest.mock('../../services/prismic')
 jest.mock('next-auth/react')
 
@@ -25,11 +30,6 @@ describe('Post page', () => {
   })
 
   it('redirects the user in case of unsubscribed', async () => {
-    const req = null
-    const params = {
-      slug: '',
-    }
-
     const getSessionMocked = mocked(getSession)
 
     getSessionMocked.mockResolvedValueOnce({
@@ -41,8 +41,52 @@ describe('Post page', () => {
     expect(response).toEqual(
       expect.objectContaining({
         redirect: expect.objectContaining({
-          destination: `/posts/preview/`,
+          destination: `/posts/preview/${params.slug}`,
         }),
+      }),
+    )
+  })
+
+  it('loads initial data', async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient)
+    const getSessionMocked = mocked(getSession)
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            {
+              type: 'heading',
+              text: 'My new post',
+            },
+          ],
+          content: [
+            {
+              type: 'paragraph',
+              text: 'My new post content',
+            },
+          ],
+        },
+        last_publication_date: '2022-04-05',
+      }),
+    } as any)
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake-active-subscription',
+    } as any)
+
+    const response = await getServerSideProps({ req, params } as any)
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: expect.objectContaining({
+            slug: 'my-slug-mocked',
+            title: 'My new post',
+            content: '<p>My new post content</p>',
+            updatedAt: '04 de abril de 2022',
+          }),
+        },
       }),
     )
   })
