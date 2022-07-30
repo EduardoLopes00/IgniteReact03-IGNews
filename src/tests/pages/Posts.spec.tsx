63a156647342d2d809e57dd0f8d2165e2561a325
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-07-30 00:32:32 -0300

    Adding getStaticProps test on the Posts page

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
+import Posts, { Post, getStaticProps } from '../../pages/posts'
 
-import Posts, { Post } from '../../pages/posts'
+import { getPrismicClient } from '../../services/prismic'
 
 const posts: Post[] = [
   {
@@ -11,36 +13,59 @@ const posts: Post[] = [
   },
 ]
 
-jest.mock('../../services/stripe')
+jest.mock('../../services/prismic')
 
 describe('Posts page', () => {
   it('render correctly', () => {
-    const { debug } = render(<Posts posts={posts} />)
-
-    debug()
+    render(<Posts posts={posts} />)
 
     expect(screen.getByText('my post')).toBeInTheDocument()
   })
 
-  //   it('loads initial data', async () => {
-  //     const retrieveStripePricesMocked = mocked(stripe.prices.retrieve)
+  it('Loads the posts', async () => {
+    const getPrismicClientMock = mocked(getPrismicClient)
 
-  //     retrieveStripePricesMocked.mockResolvedValueOnce({
-  //       id: 'fake-price-id',
-  //       unit_amount: 1000,
-  //     } as any)
+    getPrismicClientMock.mockReturnValue({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: 'test-uid',
+            data: {
+              title: [
+                {
+                  type: 'heading',
+                  text: 'My new post',
+                },
+              ],
+              content: [
+                {
+                  type: 'paragraph',
+                  text: 'My new post excerpt',
+                },
+              ],
+            },
 
-  //     const response = await getStaticProps({})
+            last_publication_date: '2022-04-05',
+          },
+        ],
+      }),
+    } as any)
 
-  //     expect(response).toEqual(
-  //       expect.objectContaining({
-  //         props: {
-  //           product: {
-  //             priceId: 'fake-price-id',
-  //             amount: '$10.00',
-  //           },
-  //         },
-  //       }),
-  //     )
-  //   })
+    const response = await getStaticProps({})
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          posts: [
+            {
+              slug: 'test-uid',
+              title: 'My new post',
+              excerpt: 'My new post excerpt',
+              updatedAt: '04 de abril de 2022',
+            },
+          ],
+        },
+      }),
+    )
+  })
 })
