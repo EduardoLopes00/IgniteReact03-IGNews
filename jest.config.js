2144c5ac0332b37a06d48d743b1eb24a738fb1f4
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-08-01 03:22:53 -0300

    Configuring coverage report and add testingPlayground

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-};
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.tsx',
+    '!src/**/*.spec.tsx',
+    '!src/**/_app.spec.tsx',
+    '!src/**/_document.spec.tsx',
+  ],
+  coverageReporters: ['lcov', 'json'],
+}
--- src/components/Async/Async.spec.tsx
+++ src/components/Async/Async.spec.tsx
@@ -1,10 +1,13 @@
-import { getByText, render, screen } from '@testing-library/react'
+import { getByText, render, screen, waitFor } from '@testing-library/react'
 import { Async } from '.'
 
 it('renders correctly', async () => {
   render(<Async />)
 
   expect(screen.getByText('Hello World')).toBeInTheDocument()
-  expect(screen.queryByText('My name is P tag!')).toBeInTheDocument()
   expect(await screen.findByText('Button')).toBeInTheDocument()
+
+  waitFor(() => {
+    expect(screen.queryByText('My name is P tag!')).toBeInTheDocument()
+  })
 })
