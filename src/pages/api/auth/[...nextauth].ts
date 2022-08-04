3a562bf6ca8c062002827f68f36ff78a492b8042
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-08-03 23:51:33 -0300

    adding the nextauth_secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import {query as q} from 'faunadb'
+import { query as q } from 'faunadb'
 
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
@@ -6,91 +6,75 @@ import GithubProvider from 'next-auth/providers/github'
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-            authorization: {
-                params: {
-                    scope: 'read:user'
-                }
-            }
-        })
-    ],
-    callbacks: {
-        async session({ session }) {
-            
-            try {
-                const userActiveSubscription = await fauna.query(
-                    q.Get(
-                        q.Intersection([ //Intersection is like the Inner Join from fauna. It either has the Union, that's like the full join 
-                            q.Match(
-                                q.Index('subscription_by_user_ref'),
-                                q.Select(
-                                    "ref",
-                                    q.Get(
-                                        q.Match(
-                                          q.Index('user_by_email'),
-                                          q.Casefold(session.user.email)
-                                        )
-                                    )
-                                )
-                            ),
-                            q.Match(
-                                q.Index('subscription_by_status'),
-                                "active"
-                            )
-                        ]) 
-                    )
-                )
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              //Intersection is like the Inner Join from fauna. It either has the Union, that's like the full join
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email),
+                    ),
+                  ),
+                ),
+              ),
+              q.Match(q.Index('subscription_by_status'), 'active'),
+            ]),
+          ),
+        )
 
-                return {
-                    ...session,
-                    activeSubscription: userActiveSubscription
-                
-                }
-            } catch {
-                return {
-                    ...session,
-                    activeSubscription: null                
-                }
-            }
-            
-        },
-        async signIn({ user }) {
-            const { email } = user;
-            
-            try {
-                //Query that verify wheter the user exists, and in case of negative, insert the new user. Otherwise, get the information about it.
-                await fauna.query(
-                    q.If(
-                        q.Not(
-                            q.Exists(
-                                q.Match(
-                                    q.Index('user_by_email'),
-                                    q.Casefold(user.email)
-                                )
-                            )
-                        ),
-                        q.Create(
-                            q.Collection('users'),
-                            { data: { email } }
-                        ),
-                        q.Get(
-                            q.Match(
-                                q.Index('user_by_email'),
-                                q.Casefold(user.email)
-                            )
-                        )
-                    )
-                )
-            } catch(err) {
-                console.log(err)
-                return false;
-            }
-            
-            
-            return true;
-        },
-    }
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
+    },
+    async signIn({ user }) {
+      const { email } = user
+
+      try {
+        //Query that verify wheter the user exists, and in case of negative, insert the new user. Otherwise, get the information about it.
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email)),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
+          ),
+        )
+      } catch (err) {
+        console.log(err)
+        return false
+      }
+
+      return true
+    },
+  },
 })
